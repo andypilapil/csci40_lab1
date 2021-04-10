66235e3050fe75d0a751ec64de64f1da9c062d3d
Andrea Pilapil <andrea.pilapil@obf.ateneo.edu>
2021-04-10 21:13:52 +0800

    forms and models commit

--- lab1/forms.py
+++ lab1/forms.py
@@ -1,4 +1,39 @@
 from django import forms
 
-class NameForm(forms.Form):
-	name = forms.CharField(label='Enter your name here', max_length=100)
+from .models import *
+
+class NameForm(forms.ModelForm):
+	class Meta:
+		model = MyModel
+		fields = ['name']
+
+class ProfileNicknameForm(forms.ModelForm):
+	class Meta:
+		model = ProfileModel
+		fields = ['profile_nickname']
+
+class ProfileBioForm(forms.ModelForm):
+	class Meta:
+		model = ProfileModel
+		fields = ['profile_bio']
+
+class ProfileImageForm(forms.ModelForm):
+	class Meta:
+		model = ProfileImageModel
+		fields = ['image']
+
+class KeyUpdateForm(forms.ModelForm):
+	class Meta:
+		model = KeyModel
+		fields = ['key', 'key_description']
+
+class ThisWeekUpdateForm(forms.ModelForm):
+	class Meta:
+		model = ThisWeekModel
+		fields = ['item_type', 'item_details']
+
+class TodayUpdateForm(forms.ModelForm):
+	class Meta:
+		model = TodayModel
+		fields = ['item_type', 'item_details']
+
--- lab1/models.py
+++ lab1/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from PIL import Image
+from model_utils import Choices
+ 
 # Create your models here.
+class MyModel(models.Model):
+	name = models.CharField(max_length=50)
+
+class ProfileModel(models.Model):
+	profile_nickname = models.CharField(max_length=20)
+	profile_bio = models.CharField(max_length=100)
+
+class ProfileImageModel(models.Model):
+	image = models.ImageField(upload_to="media")
+
+class KeyModel(models.Model): 
+	key = models.CharField(max_length=20)
+	key_description = models.CharField(max_length=50)
+
+class ThisWeekModel(models.Model):
+	ITEM_TYPES = Choices('Task', 'Event', 'Note')
+	item_type = models.CharField(choices=ITEM_TYPES, max_length=20, blank=True, null=True)
+	item_details = models.CharField(max_length=50)
+
+class TodayModel(models.Model):
+	ITEM_TYPES = Choices('Task', 'Event', 'Note')
+	item_type = models.CharField(choices=ITEM_TYPES, max_length=20, blank=True, null=True)
+	item_details = models.CharField(max_length=50)
